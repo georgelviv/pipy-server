d154c21b91fc66068dca61ab41393175991b8b79
lvivgeorge <lvivgeorge@gmail.com>
2018-04-28 17:17:55 +0300

    #2

---
+++ src/common/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Layout } from 'common';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    );
+  }
+}
+
+export { App };
